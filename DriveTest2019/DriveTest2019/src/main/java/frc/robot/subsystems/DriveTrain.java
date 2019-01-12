dfd928e7019014a8d265769230679f5f91133ae6
Deken <dsupright04@gmail.com>
2019-01-12 16:07:00 -0500

    Initial Attempt

---
+++ DriveTest2019/DriveTest2019/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,28 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class DriveTrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+    private final DifferentialDrive _drive = new DifferentialDrive(new PWMVictorSPX(1), new PWMVictorSPX(0));
+  
+    public void Drive(XboxController stick)
+    {
+        double speed = -stick.getY(Hand.kLeft);
+        double steer = stick.getX(Hand.kLeft);
+        if (speed<0.15 && speed>-0.15) speed = 0;
+        if (steer<0.15 && steer>-0.15) steer = 0;
+        _drive.arcadeDrive(speed, steer, false);
+    }
+    @Override
+    public void initDefaultCommand() {
+    }
+}
